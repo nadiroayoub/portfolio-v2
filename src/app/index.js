3c1b3d4ff6875f029a08aced44dbe50dea6c76be
Ayoub.Nadir <ayoub.nadir@atrebo.com>
2024-07-24 15:08:06 +0200

    Update cursor design to arrow and remove unused HTML elements

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,18 @@
 import "../public/images/_un-optimized/logo.svg";
 import "../style/sass/main.scss";
 
-document.addEventListener('DOMContentLoaded', () => {
-    AOS.init();
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
+});
 
-  
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('.project-demo, .project-source');
-    
-    links.forEach(link => {
-      if (link.getAttribute('href') === null) {
-        link.classList.add('invalid-link');
-      }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(
+    ".project-demo, .project-source, .project-link"
+  );
+
+  links.forEach((link) => {
+    if (link.getAttribute("href") === null) {
+      link.classList.add("invalid-link");
+    }
   });
-  
+});
