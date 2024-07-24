220f36bda53e50b9e5cd52a9f2fc0d0103b87613
Ayoub.Nadir <ayoub.nadir@atrebo.com>
2024-07-24 02:10:41 +0200

    add new log and change links projects, improve styles

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,18 @@
 import "../public/images/_un-optimized/logo.svg";
 import "../style/sass/main.scss";
+
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init();
+  });
+
+  
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.project-demo, .project-source');
+    
+    links.forEach(link => {
+      if (link.getAttribute('href') === null) {
+        link.classList.add('invalid-link');
+      }
+    });
+  });
+  
